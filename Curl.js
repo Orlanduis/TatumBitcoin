e8ba1b5a42a9214daf3acd0a8aa3d907787869b6
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-10-25 06:06:54 -0600

    Create Curl.js

---
+++ Curl.js
@@ -0,0 +1,6 @@
+curl --request POST \
+--url 'https://bitcoin-mainnet.gateway.tatum.io/' \
+--header 'accept: application/json' \
+--header 'content-type: application/json' \
+--header 'x-api-key: t-671b807575ca59dbcc582b6d-8301192144754689a0bf0e2a' \
+--data '{"jsonrpc":"2.0","method":"getblockcount","id":1}'
